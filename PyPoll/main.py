3865e10825b5403b99944faeb1fc4623dee3d638
shabs12377 <133574571+shabs12377@users.noreply.github.com>
2023-06-01 22:26:41 +0100

    Updated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,14 +4,17 @@ csvpath = os.path.join("Resources", "election_data.csv")
 #the total number of votes cast
 total_votes = 1
 #list of candidates who recieved votes
-candidates_dictionary = {}
+candidates_list = []
 #percentage of candidates won
-winning_candidates = ()
+
+candidate_dict={}
+
+winning_candidates = []
 #total number of votes candidates won
 number_of_winning_votes = ()
 #winner of election based on popular votes
 popular_vote = ()
-
+winning_count=0
 
 with open(csvpath, encoding='UTF-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -19,29 +22,28 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     # skip the first row for total number of votes cast
     # add first row's vote to net vote
  
-    
 
-    
+
+
     for row in csvreader:
         total_votes = total_votes + 1
         candidate=row[2]
         county=row[1]
-        if candidate in candidates_dictionary:
-            candidates_dictionary[candidate]+=1
-        else:  
-            candidates_dictionary[candidate]=1
-
+        if candidate not in candidates_list:
+            candidates_list.append(candidate)
+            candidate_dict[candidate]=0
+        candidate_dict[candidate]=candidate_dict[candidate]+1
+    print("Total Votes"+"\n"+str(total_votes))
 winner_votes=0
 winner_name=""
 
 
-for key in candidates_dictionary.keys():
+for key in candidate_dict:
     candidate=key
-    votes=candidates_dictionary[key]
-    print (f"{candidate} has {votes} votes") 
-    if votes>winner_votes:
+    votes=candidate_dict.get(candidate)
+    votes_percent=float(votes)/float(total_votes)*100
+    # print (f"{candidate} has {votes} votes") 
+    if votes>winning_count:
         winner_name=candidate
-        winner_votes=votes
-print(winner_votes,winner_name)
-percentage = (winner_votes / total_votes) * 100 
-print(f"percentage of {winner_votes}") 
+        winning_count=votes
+    print(f"{candidate}:{votes_percent:.3f}") 
